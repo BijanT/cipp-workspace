75a27897ee47593331f85c0840c29778a93471f3
Bijan Tabatabai <bijan311@gmail.com>
2025-03-31 15:37:41 -0500

    scripts/plot_local_saturation: Include remote BW and update font sizes

--- scripts/plot_local_saturation.py
+++ scripts/plot_local_saturation.py
@@ -13,20 +13,26 @@ if len(sys.argv) >= 3:
 ratios = []
 total_bws = []
 local_bws = []
+remote_bws = []
 with open(filename, "r") as csvfile:
     reader = csv.DictReader(csvfile)
     for row in reader:
         ratios.append(int(row["DRAM Ratio"]))
-        total_bws.append(int(row["Total BW"]) / 1024)
-        local_bws.append(int(row["Local BW"]) / 1024)
+        total_bws.append(int(row["Total BW"]) / 1000)
+        local_bws.append(int(row["Local BW"]) / 1000)
+        remote_bws.append(int(row["Remote BW"]) / 1000)
 
-plt.plot(ratios, total_bws, label="Total")
-plt.plot(ratios, local_bws, label="Local")
+plt.figure(figsize=(10,6))
+plt.plot(ratios, local_bws, label="Local", linewidth=2.0)
+plt.plot(ratios, remote_bws, label="Remote", linewidth=2.0)
+plt.plot(ratios, total_bws, label="Total", linewidth=2.0)
 
-plt.legend()
-plt.xlabel("Percent of Data in Local Memory", fontsize=14)
-plt.ylabel("Bandwidth (GB/s)", fontsize=14)
-plt.title("Peak Bandwidth Utilization By Interleave Ratio", fontsize=16)
+plt.legend(fontsize=18)
+plt.xticks(fontsize=18)
+plt.yticks(fontsize=18)
+plt.xlabel("Percent of Data in Local Memory", fontsize=18)
+plt.ylabel("Bandwidth (GB/s)", fontsize=18)
+plt.title("Peak Bandwidth Utilization By Interleave Ratio", fontsize=22)
 
 if outfile is not None:
     plt.savefig(outfile)
